9ceb3acb5b1570f71bde9187d6f0668bdb5d7b11
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-05-09 12:19:49 +0530

    Organized crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -1,7 +1,17 @@
-crave run --no-patch -- "rm -rf .repo/local_manifests; \
+#!/bin/bash
+
+rm -rf .repo/local_manifests; \
+
+# ROM source repo
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs; \
+
+# Local manifest
 git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b crdroid11 .repo/local_manifests; \ 
+
+# Re-sync
 /opt/crave/resync.sh; \
+
+# Building ROM
 source build/envsetup.sh; \
 breakfast hawao userdebug; \ 
 mka bacon"
