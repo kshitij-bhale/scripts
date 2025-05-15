643bcf7284aaa5e2a738cb4865b2bbdc0f37607e
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-05-15 22:33:18 +0530

    Create clover-15.sh

---
+++ clover-15.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Clean old manifests
+rm -rf .repo/local_manifests
+
+# Initialize AxionAOSP source
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 15-qpr2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local manifest
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Use Crave's sync method
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
+
+# KernelSU Next setup
+cd kernel/motorola/sm6225
+echo "======== Inside kernel/motorola/sm6225 ========"
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+echo "======== Added KSU successfully ========"
+cd ../../..
+echo "======== Returned to root directory ========"
+
+# Environment setup
+source build/envsetup.sh
+echo "======== Environment setup done ========"
+
+# Generate private signing keys
+gk -s
+echo "======== Private key generated ========"
+
+# Device + GApps variant lunch
+lunch clover_hawao-bp1a-userdebug
+echo "======== Lunch command done ========"
+
+# Start the build with full CPU usage
+mka clover
