2bdd74932dcb41a5e93d2039c8e8a194047d51c8
kshitij-bhale <kshitij.bhale@gmail.com>
2025-05-24 11:24:07 +0530

    Experimental commit

---
+++ experimental.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+echo "==========Clean up done=========="
+
+# ROM source repo
+repo init -u https://github.com/Miku-UI/manifesto -b Vampire_v3 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifest
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b miku_ui .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Re-sync
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
+
+# qcom-caf thing
+rm -rf hardware/qcom-caf/common
+git clone https://github.com/LineageOS/android_hardware_qcom-caf_common --depth=1 hardware/qcom-caf/common
+echo "========= qcom-caf thing done ========="
+
+# Add KSU next
+cd kernel/motorola/sm6225
+echo "======== Inside kernel/motorola/sm6225 ========"
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+echo "======== Added KSU successfully ========"
+cd ../../..
+echo "======== changed directory ========"
+
+# Building ROM
+source build/envsetup.sh
+echo "======== Environment setup done ========"
+lunch miku_hawao-bp1a-userdebug
+echo "======== Lunched ========"
+make diva
