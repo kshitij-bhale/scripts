9c93362ee3523242f22d37559cc8c0985cf85f45
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-06-02 09:22:44 +0530

    mido: BringUp for witaqua

--- witaqua.sh
+++ witaqua.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 
 # Clean old manifests
-#rm -rf .repo/local_manifests
+rm -rf .repo/local_manifests
 
 # Initialize WitAqua source
-#repo init -u https://github.com/WitAqua/manifest.git -b 15.2 --git-lfs
-#echo "=================="
-#echo "Repo init success"
-#echo "=================="
+repo init -u https://github.com/WitAqua/manifest.git -b 15.2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # Clone local manifest
-#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b witaqua-15 .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b witaqua-15 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Use Crave's sync method
-#/opt/crave/resync.sh
-#echo "======== Synced Successfully ========"
-
-# KernelSU Next setup
-#cd kernel/motorola/sm6225
-#echo "======== Inside kernel/motorola/sm6225 ========"
-#curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-#echo "======== Added KSU successfully ========"
-#cd ../../..
-#echo "======== Returned to root directory ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # Environment setup
 source build/envsetup.sh
 echo "======== Environment setup done ========"
 
 # Device + GApps variant lunch
-lunch lineage_rhode-bp1a-userdebug
+lunch lineage_mido-bp1a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
