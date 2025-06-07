5081ab0a88830180fe9d1af715ffc511ef3dae2f
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-06-07 11:50:53 +0530

    Update witaqua.sh

--- witaqua.sh
+++ witaqua.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
 # Clean old manifests
-#rm -rf .repo/local_manifests
+rm -rf .repo/local_manifests
+rm -rf kernel/motorola/sm6225
 
 # Initialize WitAqua source
-#repo init -u https://github.com/WitAqua/manifest.git -b 15.2 --git-lfs
-#echo "=================="
-#echo "Repo init success"
+repo init -u https://github.com/WitAqua/manifest.git -b 15.2 --git-lfs
+echo "=================="
+echo "Repo init success"
 #echo "=================="
 
 # Clone local manifest
-#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b witaqua-15 .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b witaqua-15 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Use Crave's sync method
-#/opt/crave/resync.sh
-#echo "======== Synced Successfully ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
-#cd kernel/motorola/sm6225
-#echo "======== Inside kernel/motorola/sm6225 ========"
-#curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-#echo "======== Added KSU successfully ========"
-#cd ../../..
-#echo "======== Returned to root directory ========"
+cd kernel/motorola/sm6225
+echo "======== Inside kernel/motorola/sm6225 ========"
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+echo "======== Added KSU successfully ========"
+cd ../../..
+echo "======== Returned to root directory ========"
 
 # Environment setup
 source build/envsetup.sh
 echo "======== Environment setup done ========"
 
-# Device + GApps variant lunch
-lunch lineage_rhode-bp1a-userdebug
+# lunch
+lunch lineage_hawao-bp1a-userdebug
 echo "======== Lunch command done ========"
 
-# Start the build with full CPU usage
+# Start the build
 mka bacon
