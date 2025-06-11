8b4bada12bcd06c2552c5a1c260a850643250a72
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-06-11 11:06:19 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -4,28 +4,28 @@
 rm -rf .repo/local_manifests
 
 # Initialize AxionAOSP source
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+#repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
+#echo "=================="
+#echo "Repo init success"
+#echo "=================="
 
 # Clone local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
+#echo "============================"
+#echo "Local manifest clone success"
+#echo "============================"
 
 # Use Crave's sync method
-/opt/crave/resync.sh
-echo "======== Synced Successfully ========"
+#/opt/crave/resync.sh
+#echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
-cd kernel/motorola/sm6225
-echo "======== Inside kernel/motorola/sm6225 ========"
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-echo "======== Added KSU successfully ========"
-cd ../../..
-echo "======== Returned to root directory ========"
+#cd kernel/motorola/sm6225
+#echo "======== Inside kernel/motorola/sm6225 ========"
+#curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+#echo "======== Added KSU successfully ========"
+#cd ../../..
+#echo "======== Returned to root directory ========"
 
 # Environment setup
 source build/envsetup.sh
@@ -36,8 +36,8 @@ gk -s
 echo "======== Private key generated ========"
 
 # Device + GApps variant lunch
-lunch lineage_hawao-bp1a-userdebug
+lunch lineage_devon-bp1a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
-ax -br
+ax -b
