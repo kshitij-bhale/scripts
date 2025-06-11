d90a36eb52d5e46b244c441c48ecd48b7126503e
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-06-11 20:02:16 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -4,10 +4,10 @@
 #rm -rf .repo/local_manifests
 
 # Initialize AxionAOSP source
-#repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
-#echo "=================="
-#echo "Repo init success"
-#echo "=================="
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.2 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # Clone local manifest
 #git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
@@ -16,8 +16,8 @@
 #echo "============================"
 
 # Use Crave's sync method
-#/opt/crave/resync.sh
-#echo "======== Synced Successfully ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
 #cd kernel/motorola/sm6225
@@ -36,7 +36,7 @@ gk -s
 echo "======== Private key generated ========"
 
 # Device + GApps variant lunch
-lunch lineage_devon-bp1a-userdebug
+lunch lineage_rhode-bp1a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
