182d2a4c6d3248385d6d343bf4dd7d6354dcec5e
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-06-14 18:06:42 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# rm -rf .repo/local_manifests
+rm -rf .repo/local_manifests
+rm -rf device/motorola/hawao
 
 # ROM source repo
-#repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
-#echo "=================="
-#echo "Repo init success"
-#echo "=================="
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # Local manifest
-#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b crdroid-11 .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b crdroid-11 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Re-sync
-#/opt/crave/resync.sh
-#echo "======== Synced Successfully ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # Add KSU next
 #cd kernel/motorola/sm6225
