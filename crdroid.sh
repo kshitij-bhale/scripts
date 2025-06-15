a2b8a2f58cbe075d8e7a3b7e6b53fdfd9bead45a
kshitij-bhale <156404280+kshitij-bhale@users.noreply.github.com>
2025-06-15 09:52:37 +0530

    initial commit

--- crdroid.sh
+++ crdroid.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
-rm -rf device/motorola/hawao
-
 # ROM source repo
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
-# Local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b crdroid-11 .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
-
 # Re-sync
 /opt/crave/resync.sh
 echo "======== Synced Successfully ========"
 
-# Add KSU next
-#cd kernel/motorola/sm6225
-#echo "======== Inside kernel/motorola/sm6225 ========"
-#curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-#echo "======== Added KSU successfully ========"
-#cd ../../..
-#echo "======== changed directory ========"
-
 # Building ROM
 source build/envsetup.sh
-echo "======== Environment setup done ========"
-breakfast hawao userdebug
-echo "======== Lunched ========"
-mka bacon
+lunch sdk_phone64_arm64-userdebug
+mka emu_img_zip
