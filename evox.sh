5872bd6d882977850ad612cc0e8a6c1961f6311a
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-06-30 21:41:42 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -3,7 +3,7 @@
 # Clean old manifests
 rm -rf .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86
-rm -rf hardware/qcom-caf/sm8250/audio/adsprpcd
+rm -rf hardware/qcom/audio
 
 # Initialize EvoX source
 repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
@@ -13,7 +13,7 @@ echo "=================="
 
 # Clone local manifest
 git clone https://github.com/kshitij-bhale/local_manifests --depth=1 -b evo-16 .repo/local_manifests
-rm -rf hardware/qcom-caf/sm8250/audio
+rm -rf hardware/qcom/audio
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
