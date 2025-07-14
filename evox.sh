63a0fe450da4cf7b52e35f642d6ac26f368e7a28
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-14 08:42:00 +0530

    hawao: Added motocam

--- evox.sh
+++ evox.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
 
 # Clean old manifests
-rm -rf .repo/local_manifests
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf .repo/local_manifests
+# rm -rf prebuilts/clang/host/linux-x86
 
 # Initialize EvoX source
-repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+# repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+# echo "=================="
+# echo "Repo init success"
+# echo "=================="
 
-# Clone local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth=1 -b evo-16 .repo/local_manifests
-rm -rf hardware/qcom/audio
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+# # Clone local manifest
+# git clone https://github.com/kshitij-bhale/local_manifests --depth=1 -b evo-16 .repo/local_manifests
+# rm -rf hardware/qcom/audio
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
+
+# MotoCam
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera4-bengal.git -b android-15 vendor/motorola/MotCamera4-bengal
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera-common.git -b android-15 vendor/motorola/MotCamera-common
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotoPhotoEditor.git -b android-15 vendor/motorola/MotoPhotoEditor
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera3AI-bengal.git -b android-15 vendor/motorola/MotCamera3AI-bengal
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCameraAI-common.git -b android-15 vendor/motorola/MotCameraAI-common
+git clone https://github.com/Deivid-21s-Sources/proprietary_vendor_motorola_MotoSignatureApp.git -b android-15 vendor/motorola/MotoSignatureApp
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotorolaSettingsProvider.git -b android-15 vendor/motorola/MotorolaSettingsProvider
 
 # Use Crave's sync method
 /opt/crave/resync.sh
 echo "======== Synced Successfully ========"
 
-# KernelSU Next setup
-cd kernel/motorola/sm6225
-echo "======== Inside kernel/motorola/sm6225 ========"
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-echo "======== Added KSU successfully ========"
-cd ../../..
-echo "======== Returned to root directory ========"
+# # KernelSU Next setup
+# cd kernel/motorola/sm6225
+# echo "======== Inside kernel/motorola/sm6225 ========"
+# curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+# echo "======== Added KSU successfully ========"
+# cd ../../..
+# echo "======== Returned to root directory ========"
 
 # Environment setup
 source build/envsetup.sh
