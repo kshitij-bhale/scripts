67e94060a88d5cff0b8e8fc42ff2defc029e7c48
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-15 19:52:44 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -27,7 +27,7 @@ git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera-comm
 git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotoPhotoEditor.git -b android-15 vendor/motorola/MotoPhotoEditor
 git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera3AI-bengal.git -b android-15 vendor/motorola/MotCamera3AI-bengal
 git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCameraAI-common.git -b android-15 vendor/motorola/MotCameraAI-common
-git clone https://github.com/Deivid-21s-Sources/proprietary_vendor_motorola_MotoSignatureApp.git -b android-15 vendor/motorola/MotoSignatureApp
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotoSignatureApp.git -b android-15 vendor/motorola/MotoSignatureApp
 git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotorolaSettingsProvider.git -b android-15 vendor/motorola/MotorolaSettingsProvider
 
 # # KernelSU Next setup
