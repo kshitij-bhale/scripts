ed84f4cdf0e09a389569a81e5fc5383291ba7744
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-15 07:42:42 +0530

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -3,20 +3,24 @@
 # Clean old manifests
 rm -rf .repo/local_manifests
 rm -rf device/motorola/hawao
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf prebuilts/clang/host/linux-x86
 
 # Initialize EvoX source
-# repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
-# echo "=================="
-# echo "Repo init success"
-# echo "=================="
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # # Clone local manifest
 git clone https://github.com/kshitij-bhale/local_manifests --depth=1 -b evo-16 .repo/local_manifests
-# rm -rf hardware/qcom/audio
+rm -rf hardware/qcom/audio
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
+
+# Use Crave's sync method
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # MotoCam
 git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera4-bengal.git -b android-15 vendor/motorola/MotCamera4-bengal
@@ -27,17 +31,13 @@ git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCameraAI-co
 git clone https://github.com/Deivid-21s-Sources/proprietary_vendor_motorola_MotoSignatureApp.git -b android-15 vendor/motorola/MotoSignatureApp
 git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotorolaSettingsProvider.git -b android-15 vendor/motorola/MotorolaSettingsProvider
 
-# Use Crave's sync method
-/opt/crave/resync.sh
-echo "======== Synced Successfully ========"
-
 # # KernelSU Next setup
-# cd kernel/motorola/sm6225
-# echo "======== Inside kernel/motorola/sm6225 ========"
-# curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-# echo "======== Added KSU successfully ========"
-# cd ../../..
-# echo "======== Returned to root directory ========"
+cd kernel/motorola/sm6225
+echo "======== Inside kernel/motorola/sm6225 ========"
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+echo "======== Added KSU successfully ========"
+cd ../../..
+echo "======== Returned to root directory ========"
 
 # Environment setup
 source build/envsetup.sh
