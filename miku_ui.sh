05d20395d83a0045ff65ccba75220667c25f6f69
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-18 07:48:20 +0530

    Update miku_ui.sh

--- miku_ui.sh
+++ miku_ui.sh
@@ -20,24 +20,6 @@ echo "============================"
 /opt/crave/resync.sh
 echo "======== Synced Successfully ========"
 
-echo "======== Nuking all test modules from art/ ========"
-
-# Remove all art/ test directories
-find art -type d -name test | xargs rm -rf
-
-# Patch all Android.bp under art/ to comment out test modules
-find art -name Android.bp | while read -r bp; do
-  sed -i 's/^\(\s*\)name: *"art_.*_tests"/\1\/\/ &/' "$bp"
-  sed -i 's/^\(\s*\)name: *"art_standalone_.*_tests"/\1\/\/ &/' "$bp"
-  sed -i 's/^\(\s*\)name: *"lib.*-gtest"/\1\/\/ &/' "$bp"
-  sed -i 's/^\(\s*\)name: *".*_gtest_defaults"/\1\/\/ &/' "$bp"
-  sed -i 's/^\(\s*\)name: *".*_test_defaults"/\1\/\/ &/' "$bp"
-  sed -i 's/^\(\s*\)test_config_template:/\1\/\/ &/' "$bp"
-done
-
-# set skip-tests flag
-export WITHOUT_TESTS=true
-
 # Add KSU next
 cd kernel/motorola/sm6225
 echo "======== Inside kernel/motorola/sm6225 ========"
