e3779d1fc444d67aa4b6f493cb0b3fa24afbc7ef
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-21 10:20:23 +0530

    Update clover-16.sh

--- clover-16.sh
+++ clover-16.sh
@@ -11,14 +11,14 @@
 #echo "=================="
 
 # Clone local manifest
-#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover-16 .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover-16 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Use Crave's sync method
-#/opt/crave/resync.sh
-#echo "======== Synced Successfully ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # MotoCam
 #git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera4-bengal.git -b android-15 vendor/motorola/MotCamera4-bengal
