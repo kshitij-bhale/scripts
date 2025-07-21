cb7071f3f1d44f16501637d31f2900027d66b2e8
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-21 22:54:15 +0530

    Update clover-16.sh

--- clover-16.sh
+++ clover-16.sh
@@ -5,10 +5,10 @@
 # rm -rf prebuilts/clang/host/linux-x86
 
 # Initialize Clover porject source
-repo init -u https://github.com/The-Clover-Project/manifest.git -b 16 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+# repo init -u https://github.com/The-Clover-Project/manifest.git -b 16 --git-lfs
+# echo "=================="
+# echo "Repo init success"
+# echo "=================="
 
 # Clone local manifest
 # git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover-16 .repo/local_manifests
@@ -17,8 +17,8 @@ echo "=================="
 # echo "============================"
 
 # Use Crave's sync method
-/opt/crave/resync.sh
-echo "======== Synced Successfully ========"
+# /opt/crave/resync.sh
+# echo "======== Synced Successfully ========"
 
 # MotoCam
 #git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera4-bengal.git -b android-15 vendor/motorola/MotCamera4-bengal
@@ -42,7 +42,7 @@ source build/envsetup.sh
 echo "======== Environment setup done ========"
 
 # Lunch
-lunch clover_devon-bp2a-userdebug
+lunch clover_hawao-bp2a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build
