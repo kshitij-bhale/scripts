bf0934396f9a8c9010f63627b745d057b6e11ef6
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-21 16:19:50 +0530

    Update clover-16.sh

--- clover-16.sh
+++ clover-16.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Clean up
-#rm -rf .repo/local_manifests
-#rm -rf prebuilts/clang/host/linux-x86
+# rm -rf .repo/local_manifests
+# rm -rf prebuilts/clang/host/linux-x86
 
 # Initialize Clover porject source
-#repo init -u https://github.com/The-Clover-Project/manifest.git -b 16 --git-lfs
-#echo "=================="
-#echo "Repo init success"
-#echo "=================="
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 16 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # Clone local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover-16 .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+# git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover-16 .repo/local_manifests
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
 
 # Use Crave's sync method
 /opt/crave/resync.sh
