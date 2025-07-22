8d55cfb6c08bee753d28bb9423a9369044faf227
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-22 12:44:51 +0530

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
-rm -rf device/motorola/hawao
+rm -rf prebuilts/clang/host/linux-x86
 
 # ROM source repo
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
@@ -19,13 +19,22 @@ echo "============================"
 /opt/crave/resync.sh
 echo "======== Synced Successfully ========"
 
+# MotoCam
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera4-bengal.git -b android-15 vendor/motorola/MotCamera4-bengal
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera-common.git -b android-15 vendor/motorola/MotCamera-common
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotoPhotoEditor.git -b android-15 vendor/motorola/MotoPhotoEditor
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCamera3AI-bengal.git -b android-15 vendor/motorola/MotCamera3AI-bengal
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotCameraAI-common.git -b android-15 vendor/motorola/MotCameraAI-common
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotoSignatureApp.git -b android-15 vendor/motorola/MotoSignatureApp
+git clone https://gitlab.com/Deivid21/proprietary_vendor_motorola_MotorolaSettingsProvider.git -b android-15 vendor/motorola/MotorolaSettingsProvider
+
 # Add KSU next
-#cd kernel/motorola/sm6225
-#echo "======== Inside kernel/motorola/sm6225 ========"
-#curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-#echo "======== Added KSU successfully ========"
-#cd ../../..
-#echo "======== changed directory ========"
+cd kernel/motorola/sm6225
+echo "======== Inside kernel/motorola/sm6225 ========"
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+echo "======== Added KSU successfully ========"
+cd ../../..
+echo "======== changed directory ========"
 
 # Building ROM
 source build/envsetup.sh
