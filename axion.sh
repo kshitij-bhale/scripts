53184f48196d347d213afd38ba4f6ff6cfe9b690
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-08-14 17:59:54 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -11,7 +11,7 @@ echo "Repo init success"
 echo "=================="
 
 # Clone local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
