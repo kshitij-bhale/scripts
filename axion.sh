628db2ab80a3e76b71f1d535f86edde13b71fb12
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-08-14 13:21:43 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Clean old manifests
-#rm -rf .repo/local_manifests
-#rm -rf prebuilts/clang/host/linux-x86
+# rm -rf .repo/local_manifests
+rm -rf prebuilts/clang/host/linux-x86
+rm -rf device/motorola/sm6225-common
 
 # Initialize AxionAOSP source
 repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
@@ -11,22 +12,22 @@ echo "Repo init success"
 echo "=================="
 
 # Clone local manifest
-#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/kshitij-bhale/android_device_motorola_sm6225-common device/motorola/sm6225-common
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
 
 # Use Crave's sync method
 /opt/crave/resync.sh
 echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
-#cd kernel/motorola/sm6225
-#echo "======== Inside kernel/motorola/sm6225 ========"
-#curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-#echo "======== Added KSU successfully ========"
-#cd ../../..
-#echo "======== Returned to root directory ========"
+# cd kernel/motorola/sm6225
+# echo "======== Inside kernel/motorola/sm6225 ========"
+# curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+# echo "======== Added KSU successfully ========"
+# cd ../../..
+# echo "======== Returned to root directory ========"
 
 # Environment setup
 source build/envsetup.sh
@@ -37,7 +38,7 @@ gk -s
 echo "======== Private key generated ========"
 
 # Device + GApps variant lunch
-lunch lineage_hawao-bp2a-userdebug
+lunch lineage_devon-bp2a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
