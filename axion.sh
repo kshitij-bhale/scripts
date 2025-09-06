7b7e830ca313ad00ba75e3975606f733f7b967ff
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-09-06 07:41:56 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Clean old manifests
-# rm -rf .repo/local_manifests
-# rm -rf hardware/motorola
+rm -rf .repo/local_manifests
+rm -rf hardware/motorola
 # rm -rf packages/resources/devicesettings
 # rm -rf prebuilts/clang/host/linux-x86
 
@@ -13,14 +13,14 @@
 # echo "=================="
 
 # Clone local manifest
-# git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
-# echo "============================"
-# echo "Local manifest clone success"
-# echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Use Crave's sync method
-# /opt/crave/resync.sh
-# echo "======== Synced Successfully ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
 # cd kernel/motorola/sm6225
@@ -39,7 +39,7 @@ gk -s
 echo "======== Private key generated ========"
 
 # Device + GApps variant lunch
-lunch lineage_hawao-bp2a-userdebug
+lunch lineage_devon-bp2a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
