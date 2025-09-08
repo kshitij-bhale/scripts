4a05ddb49f1d77e757b2d3517379b0487ed61fd7
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-09-08 21:01:48 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
-rm -rf packages/resources/devicesettings
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf .repo/local_manifests
+# rm -rf packages/resources/devicesettings
+# rm -rf prebuilts/clang/host/linux-x86
 
 # ROM source repo
-repo init -u https://github.com/kshitij-bhale/android.git -b lineage-22.2 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+# repo init -u https://github.com/kshitij-bhale/android.git -b lineage-22.2 --git-lfs
+# echo "=================="
+# echo "Repo init success"
+# echo "=================="
 
 # Local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+# git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
 
 # Re-sync
-/opt/crave/resync.sh
-echo "======== Synced Successfully ========"
+# /opt/crave/resync.sh
+# echo "======== Synced Successfully ========"
 
 # Add KSU next
 #cd kernel/motorola/sm6225
@@ -31,6 +31,6 @@ echo "======== Synced Successfully ========"
 # Building ROM
 source build/envsetup.sh
 echo "======== Environment setup done ========"
-lunch lineage_devon-bp1a-userdebug
+lunch lineage_hawao-bp1a-userdebug
 echo "======== Lunched ========"
 mka bacon
