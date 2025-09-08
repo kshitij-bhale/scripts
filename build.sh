e70baf18110d58f9cb7da50cb335fffea13508b6
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-09-08 07:45:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#rm -rf .repo/local_manifests
-#rm -rf prebuilts/clang/host/linux-x86
+rm -rf .repo/local_manifests
+rm -rf prebuilts/clang/host/linux-x86
 
 # ROM source repo
-#repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
-#echo "=================="
-#echo "Repo init success"
-#echo "=================="
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # Local manifest
-#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b lineage-22.2 .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b lineage-22.2 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Re-sync
-#/opt/crave/resync.sh
-#echo "======== Synced Successfully ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # Add KSU next
 #cd kernel/motorola/sm6225
@@ -30,6 +30,6 @@
 # Building ROM
 source build/envsetup.sh
 echo "======== Environment setup done ========"
-breakfast hawao userdebug
+lunch lineage_devon-bp1a-userdebug
 echo "======== Lunched ========"
 mka bacon
