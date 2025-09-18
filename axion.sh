f1e2e8100af09d5dc2b34eb099c6ba5412cb5950
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-09-19 00:03:48 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -5,10 +5,10 @@
 #rm -rf prebuilts/clang/host/linux-x86
 
 # # Initialize AxionAOSP source
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+# repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+# echo "=================="
+# echo "Repo init success"
+# echo "=================="
 
 # Clone local manifest
 #git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
@@ -17,8 +17,8 @@ echo "=================="
 #echo "============================"
 
 # Use Crave's sync method
-/opt/crave/resync.sh
-echo "======== Synced Successfully ========"
+# /opt/crave/resync.sh
+# echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
 # cd kernel/motorola/sm6225
