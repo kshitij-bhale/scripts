37512f79d24c4a97bdb0bc146a65d8bf79ffebec
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-09-20 11:47:52 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -2,6 +2,7 @@
 
 # Clean old manifests
 #rm -rf .repo/local_manifests
+rm -rf device/motorola/devon
 #rm -rf prebuilts/clang/host/linux-x86
 
 # # Initialize AxionAOSP source
@@ -11,14 +12,14 @@
 # echo "=================="
 
 # Clone local manifest
-#git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
-#echo "============================"
-#echo "Local manifest clone success"
-#echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b test .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Use Crave's sync method
-# /opt/crave/resync.sh
-# echo "======== Synced Successfully ========"
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
 # cd kernel/motorola/sm6225
