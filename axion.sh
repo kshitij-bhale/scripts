87f738ee7ce98e1f45832e60c59340d4108237a2
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-09-21 19:05:43 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -13,10 +13,10 @@ echo "============Clean-up=============="
 #rm -rf prebuilts/clang/host/linux-x86
 
 # # Initialize AxionAOSP source
-# repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
-# echo "=================="
-# echo "Repo init success"
-# echo "=================="
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
 
 # Clone local manifest
 git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
