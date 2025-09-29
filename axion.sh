4dad057bfc2bcf8567c26977de5695dfe34b2ab7
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-09-29 22:54:34 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Clean old manifests
-rm -rf .repo/local_manifests
-rm -rf prebuilts/clang/host/linux-x86
-rm -rf hardware/motorola
+# rm -rf .repo/local_manifests
+# rm -rf prebuilts/clang/host/linux-x86
+# rm -rf hardware/motorola
 
 # # Initialize AxionAOSP source
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+# repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs
+# echo "=================="
+# echo "Repo init success"
+# echo "=================="
 
 # Clone local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+# git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
 
 # Use Crave's sync method
-/opt/crave/resync.sh
-echo "======== Synced Successfully ========"
+# /opt/crave/resync.sh
+# echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
 # cd kernel/motorola/sm6225
@@ -38,7 +38,7 @@ gk -s
 echo "======== Private key generated ========"
 
 # Device + GApps variant lunch
-lunch lineage_devon-bp2a-userdebug
+lunch lineage_hawao-bp2a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
