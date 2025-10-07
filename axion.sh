e82d87b3a526103703b86e1efa83db923684cdf2
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-10-07 19:38:18 +0530

    Update axion.sh

--- axion.sh
+++ axion.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Clean old manifests
-# rm -rf .repo/local_manifests
-# rm -rf prebuilts/clang/host/linux-x86
+rm -rf .repo/local_manifests
+rm -rf prebuilts/clang/host/linux-x86
 # rm -rf hardware/motorola
 
 # # Initialize AxionAOSP source
@@ -12,22 +12,22 @@ echo "Repo init success"
 echo "=================="
 
 # Clone local manifest
-# git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
-# echo "============================"
-# echo "Local manifest clone success"
-# echo "============================"
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b axion .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
 
 # Use Crave's sync method
 /opt/crave/resync.sh
 echo "======== Synced Successfully ========"
 
 # KernelSU Next setup
-# cd kernel/motorola/sm6225
-# echo "======== Inside kernel/motorola/sm6225 ========"
-# curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
-# echo "======== Added KSU successfully ========"
-# cd ../../..
-# echo "======== Returned to root directory ========"
+cd kernel/motorola/sm6225
+echo "======== Inside kernel/motorola/sm6225 ========"
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+echo "======== Added KSU successfully ========"
+cd ../../..
+echo "======== Returned to root directory ========"
 
 # Environment setup
 source build/envsetup.sh
