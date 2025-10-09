c7df7279ff30bc11135a41e5b4dd14e0a0e1b91c
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-10-09 19:57:14 +0530

    Create voltage.sh

---
+++ voltage.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Clean old manifests
+rm -rf .repo/local_manifests
+rm -rf prebuilts/clang/host/linux-x86
+
+# # Initialize AxionAOSP source
+repo init -u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local manifest
+git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b voltage .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Use Crave's sync method
+/opt/crave/resync.sh
+echo "======== Synced Successfully ========"
+
+# KernelSU Next setup
+cd kernel/motorola/sm6225
+echo "======== Inside kernel/motorola/sm6225 ========"
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next/kernel/setup.sh" | bash -
+echo "======== Added KSU successfully ========"
+cd ../../..
+echo "======== Returned to root directory ========"
+
+# Environment setup
+source build/envsetup.sh
+echo "======== Environment setup done ========"
+
+# Device + GApps variant lunch
+brunch devon
+echo "======== Lunch command done ========"
