b2c9ef66ba91e712e09b387e966ae76b15c588e2
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-10-17 16:24:29 +0530

    Update clover.sh

--- clover.sh
+++ clover.sh
@@ -1,21 +1,16 @@
+sh
 #!/bin/bash
 
-# Clean up
+# Clean up stale directories for a fresh start
+# (Keeping this from your original script)
 rm -rf .repo/local_manifests
-rm -rf packages/apps/Settings
-rm -rf device/motorola/devon
-rm -rf .repo/projects/art.git
-rm -rf vendor/clover
 rm -rf prebuilts/clang/host/linux-x86
 
-# Initialize Clover porject source
+# Initialize Clover project source
 repo init -u https://github.com/kshitij-bhale/manifest.git -b 16 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
-
-git clone https://github.com/kshitij-bhale/packages_apps_Settings --depth=1 -b 16 packages/apps/Settings
-git clone https://github.com/kshitij-bhale/vendor_clover --depth=1 -b 16 vendor/clover
 
 # Clone local manifest
 git clone https://github.com/kshitij-bhale/local_manifests --depth=1 -b clover .repo/local_manifests
