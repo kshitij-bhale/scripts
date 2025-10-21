958d28b31b3273f14d4a55f1be484529249bc981
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-10-21 23:07:10 +0530

    Comment out various commands in clover.sh
    Comment out commands for cleaning, initializing, and cloning repositories.

--- clover.sh
+++ clover.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Clean old manifests
-rm -rf .repo/local_manifests
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf .repo/local_manifests
+# rm -rf prebuilts/clang/host/linux-x86
 rm -rf packages/apps/Settings
-rm -rf vendor/clover
+# rm -rf vendor/clover
 
 # # Initialize AxionAOSP source
-repo init -u https://github.com/kshitij-bhale/manifest.git -b 16 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+# repo init -u https://github.com/kshitij-bhale/manifest.git -b 16 --git-lfs
+# echo "=================="
+# echo "Repo init success"
+# echo "=================="
 
 # Clone local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+# git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover .repo/local_manifests
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
 
 # Use Crave's sync method
-/opt/crave/resync.sh
-echo "======== Synced Successfully ========"
+# /opt/crave/resync.sh
+# echo "======== Synced Successfully ========"
 
 # CLone forked repos
 git clone https://github.com/kshitij-bhale/packages_apps_Settings -b 16 packages/apps/Settings
-git clone https://github.com/kshitij-bhale/vendor_clover -b 16 vendor/clover
+# git clone https://github.com/kshitij-bhale/vendor_clover -b 16 vendor/clover
 
 # KernelSU Next setup
 # cd kernel/motorola/sm6225
@@ -39,8 +39,8 @@ source build/envsetup.sh
 echo "======== Environment setup done ========"
 
 # Device + GApps variant lunch
-lunch clover_devon-bp2a-eng
-echo "======== Lunch command done ========"
+# lunch clover_devon-bp2a-eng
+# echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
-mka clover
+m Settings
