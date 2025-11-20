d4495120d896b87d86657b7adb268e155472a797
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-11-20 22:54:49 +0530

    Update clover.sh

--- clover.sh
+++ clover.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Clean old manifests
-rm -rf .repo/local_manifests
-rm -rf device/motorola/sm6225-common
+# rm -rf .repo/local_manifests
+# rm -rf device/motorola/sm6225-common
 # rm -rf vendor/motorola/devon
 # rm -rf vendor/motorola/rhode
 # rm -rf vendor/motorola/hawao
 # rm -rf vendor/motorola/sm6225-common
 # rm -rf hardware/motorola
-rm -rf prebuilts/clang/host/linux-x86
+# rm -rf prebuilts/clang/host/linux-x86
 
 # # Initialize AxionAOSP source
 repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr1 --git-lfs --depth=1
@@ -17,10 +17,10 @@ echo "Repo init success"
 echo "=================="
 
 # Clone local manifest
-git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+# git clone https://github.com/kshitij-bhale/local_manifests --depth 1 -b clover .repo/local_manifests
+# echo "============================"
+# echo "Local manifest clone success"
+# echo "============================"
 
 # Use Crave's sync method
 /opt/crave/resync.sh
@@ -39,7 +39,7 @@ source build/envsetup.sh
 echo "======== Environment setup done ========"
 
 # Device + GApps variant lunch
-lunch clover_devon-bp3a-eng
+lunch clover_devon-bp3a-userdebug
 echo "======== Lunch command done ========"
 
 # Start the build with full CPU usage
